3f13e77c2e8ea5c8f582eb175a2798c106738144
Ruslan Nazarov <hlfan2@yandex.ru>
2023-04-27 15:46:19 +0700

    add Skate model

--- skates/admin.py
+++ skates/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from.models import Skate
+
+
+class SkateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'article_number', 'exist')  # Отображение полей
+    list_display_links = ('id', 'name')  # Установка ссылок на атрибуты
+    search_fields = ('name', 'price')  # поиск по полям
+    list_editable = ('price', 'exist')  # Изменяемое поле
+    list_filter = ('exist', 'color')  # Фильтры полей
+
+
+admin.site.register(Skate, SkateAdmin)  # (Модель, Форма админки модели)
--- skates/models.py
+++ skates/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Skate(models.Model):
+    name = models.CharField(max_length=120, default='skate', verbose_name='Название')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    price = models.FloatField(default=10, verbose_name='Цена')
+    article_number = models.IntegerField(verbose_name='Артикул')
+    color = models.CharField(max_length=30, verbose_name='Цвет')
+    date_create = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    date_update = models.DateTimeField(auto_now=True, null=True, verbose_name='Дата изменения')
+    photo = models.ImageField(upload_to='image/%Y/%m/%d', null=True, verbose_name='Фото')
+    exist = models.BooleanField(default=True, verbose_name='Доступно')
+
+    def __str__(self):
+        """Переопределение названия объекта"""
+        return self.name  # В админке возвращается название
+
+    class Meta:
+        """Класс для названия модели Skate в админке"""
+        verbose_name = 'Скейт'  # Название в единственном числе
+        verbose_name_plural = 'Скейтборды'  # Название во множественном числе
+        ordering = ['-name']  # Сортировка полей по убыванию (-)
